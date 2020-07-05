dacf99b1dbe90c5fb58b7452a876601dad02432e
tshkoushik <61496803+tshkoushik@users.noreply.github.com>
2020-07-05 14:08:31 +0530

    Add files via upload

---
+++ webpage/flaskweb/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '68a517e10f3ab14c1258d42a46b60004'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+from flaskweb import routes
